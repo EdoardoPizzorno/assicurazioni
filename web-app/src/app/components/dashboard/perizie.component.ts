8679e7f22758e4bf90238eb1c7ec007cbde19450
Edoardo Pizzorno <92369419+EdoardoPizzorno@users.noreply.github.com>
2024-04-14 20:50:31 +0200

    Improve show perizie's details

--- web-app/src/app/components/dashboard/perizie.component.ts
+++ web-app/src/app/components/dashboard/perizie.component.ts
@@ -48,38 +48,82 @@ export class PerizieComponent {
   }
 
   openInfo(perizia: any) {
-    let imagesHtml = '';
-    perizia.photos.forEach((image: any) => {
-      // Load images
-      imagesHtml += `
-      <div style="display: inline-block; border: 1px solid gray; border-radius: 5px">
-        <img src="${image.url}" alt="Image" style="width: 100px; height: 75px">
-      </div>
-      `;
-    });
-
+    let imagesHtml = this.generatePhotosHtml(perizia.photos);
 
     Swal.fire({
       title: perizia.description,
       html: `
         <b>Data:</b> ${perizia.date} alle ${perizia.time}<br>
         <b>Descrizione:</b> ${perizia.description}<br>
-        <br>
-        <div>
+        <b>Creata da:</b> <p class="${this.checkOperatorDeleted(perizia.operator.username)}"> ${perizia.operator.username} </p> <br>
+        <div class="row container">
           ${imagesHtml}
         </div>
       `,
-      imageUrl: perizia.immagine,
-      imageWidth: 400,
-      imageHeight: 200,
-      imageAlt: 'Custom image',
+      width: "60%",
     });
+
   }
 
   drag() {
     console.log("Marker dragged");
     this.markerOptions.animation = google.maps.Animation.BOUNCE;
     this.markerOptions.draggable = true;
+  }
+
+  //#endregion
+
+  //#region UTILS
+
+  generatePhotosHtml(images: any[]): string {
+    let imagesHtml = "";
+    images.forEach((image: any, index: number) => {
+      const cardId = `card-${index}`;
+      // Load images
+      imagesHtml += `
+        <div class="col-md-4">
+          <div class="card m-3">
+              <img src="${image.url}" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <p class="card-title">Fotografata da <b class="${this.checkOperatorDeleted(image.photographer)}">${image.photographer}</b></p>
+                  <a class="text-decoration-none" data-bs-toggle="collapse" href="#comments-${cardId}" role="button" aria-expanded="false" aria-controls="comments-${cardId}">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                          <path d="M7.646 10.646a.5.5 0 0 1 .708 0l4 4a.5.5 0 0 1-.708.708L8 11.707l-3.646 3.647a.5.5 0 1 1-.708-.708l4-4a.5.5 0 0 1 0-.708z"/>
+                      </svg>
+                  </a>
+                  <div class="collapse" id="comments-${cardId}">
+                      <div class="card card-body" style="font-size: 10pt; margin: 0px !important; vertical-align: middle !important;">
+                          ${this.generateCommentsHtml(image.comments)}
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div>
+      `;
+    });
+    return imagesHtml;
+  }
+
+  generateCommentsHtml(comments: any[]): string {
+    let commentsHtml = "";
+    if (comments == undefined || comments.length == 0)
+      commentsHtml += `<p>Nessun commento</p>`;
+    else {
+      comments.forEach((comment: any) => {
+        commentsHtml += `<div class="row mt-1">
+        <b class='${comment.writer == "Utente eliminato" ? "deleted" : ""}'>${comment.writer}</b>:
+        <p> &nbsp; ${comment.text}</p>
+        </div>
+        `;
+      });
+    }
+    return commentsHtml;
+  }
+
+  checkOperatorDeleted(operator: string) {
+    if (operator == undefined)
+      return "";
+    return operator == "Utente eliminato" ? "deleted" : "";
   }
 
   //#endregion
