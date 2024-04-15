c9fe4652fa49110d65a134646d293f421afb0ded
Edoardo Pizzorno <92369419+EdoardoPizzorno@users.noreply.github.com>
2024-04-15 17:02:00 +0200

    Include edit functionality and improve layout

--- web-app/src/app/components/dashboard/perizie.component.ts
+++ web-app/src/app/components/dashboard/perizie.component.ts
@@ -35,9 +35,12 @@ export class PerizieComponent {
   }
 
   async editPerizia(perizia: any) {
+    let imagesHtml = this.generatePhotosHtmlForEdit(perizia.photos);
+
     Swal.fire({
       title: "Modifica perizia",
       html: `
+      <form id="editForm" class="container">
         <div class="form-group">
           <label for="description">Descrizione</label>
           <input type="text" class="form-control" id="description" value="${perizia.description}">
@@ -50,16 +53,21 @@ export class PerizieComponent {
           <label for="time">Ora</label>
           <input type="time" class="form-control" id="time" value="${perizia.time}">
         </div>
-      `,
+        <div class="row">
+          ${imagesHtml}
+        </div>
+      </form>
+    `,
       showCancelButton: true,
       confirmButtonText: "Salva",
-      cancelButtonText: "Annulla"
+      cancelButtonText: "Annulla",
+      width: "80%"
     }).then(async (result) => {
       if (result.isConfirmed) {
         const description = (<HTMLInputElement>document.getElementById("description")).value;
         const date = (<HTMLInputElement>document.getElementById("date")).value;
         const time = (<HTMLInputElement>document.getElementById("time")).value;
-        
+
         perizia.description = description;
         perizia.date = date;
         perizia.time = time;
@@ -149,6 +157,51 @@ export class PerizieComponent {
         <p class="comment"> &nbsp; ${comment}</p>
         </div>
         `;
+      });
+    }
+    return commentsHtml;
+  }
+
+  generatePhotosHtmlForEdit(images: any[]): string {
+    let imagesHtml = "";
+    images.forEach((image: any, index: number) => {
+      const cardId = `card-${index}`;
+      // Load images
+      imagesHtml += `
+        <div class="col-md-4">
+          <div class="card m-3">
+            <img src="${image.url}" class="card-img-top" alt="..." contenteditable="true">
+            <div class="card-body">
+              <p class="card-title">Immagine fotografata da <b class="${this.checkOperatorDeleted(image.photographer)}">${image.photographer}</b></p>
+              <a class="text-decoration-none" data-bs-toggle="collapse" href="#comments-${cardId}" role="button" aria-expanded="false" aria-controls="comments-${cardId}">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                  <path d="M7.646 10.646a.5.5 0 0 1 .708 0l4 4a.5.5 0 0 1-.708.708L8 11.707l-3.646 3.647a.5.5 0 1 1-.708-.708l4-4a.5.5 0 0 1 0-.708z"/>
+                </svg>
+              </a>
+              <div class="collapse" id="comments-${cardId}">
+                <div class="card card-body text-center" style="font-size: 10pt; vertical-align: middle !important;">
+                  ${this.generateCommentsHtmlForEdit(image.comments)}
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+    return imagesHtml;
+  }
+  
+  generateCommentsHtmlForEdit(comments: any[]): string {
+    let commentsHtml = "";
+    if (comments == undefined || comments.length == 0)
+      commentsHtml += `<p class="text-center comment">Nessun commento</p>`;
+    else {
+      console.log(comments)
+      comments.forEach((comment: any) => {
+        commentsHtml += `<div class="text-center m-1">
+          <div contenteditable="true" class="comment border" role="textbox" aria-multiline="true">${comment}</div>
+          </div>
+          `;
       });
     }
     return commentsHtml;
